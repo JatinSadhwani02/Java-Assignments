5745fd7d69cec5e9ec4cfa90106f3b839192f2c7
Jatin Sadhwani <75443345+JatinSadhwani02@users.noreply.github.com>
2022-02-18 21:30:04 +0530

    Java Assignments

--- Java Assignment/Question1.java
+++ Java Assignment/Question1.java
@@ -10,21 +10,17 @@ public class Question1{
         languages.put(4, "Python");
         languages.put(5, "C++");
 
-        // Iterate using for loop
-        System.out.println("Iterate using for loop");
-        for(Map.Entry<Integer,String> data : languages.entrySet()){
-            System.out.println("Key = " + data.getKey() + ", Value = "+ data.getValue());
-        }
 
         // Iterate using forEach loop
-        System.out.println("\nIterate using forEach loop");
+        System.out.println("\nIterate using advanced forEach loop");
         languages.forEach((key, value)->{
             System.out.println("Key = " + key + ", Value = "+ value);
         });
 
         // Iterate using while loop
         System.out.println("\nIterate using While loop");
-        Iterator <Integer> itr = languages.keySet().iterator();        while(itr.hasNext()){
+        Iterator <Integer> itr = languages.keySet().iterator();        
+        while(itr.hasNext()){
             int key = (int)itr.next();
             System.out.println("Key = " + key + ", Value = "+ languages.get(key));
 
